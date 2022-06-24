3f1e5b149752fc4c90c043dcb7b9676dbc87ffa5
sukanyaharidas <sukanyaharidas97@gmail.com>
2022-06-24 22:26:39 +0530

    Update sidebar.component.ts

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -16,7 +16,7 @@ export class SidebarComponent implements OnInit {
 
   logoutUser(){
 
-    this.router.navigate(['\login']);
+    this.router.navigate(['/']);
     localStorage.removeItem('token');
    }
 

839d6bc33c5f9c74286ab9d77ab441f3b3615fc3
sukanyaharidas <sukanyaharidas97@gmail.com>
2022-06-26 07:20:19 +0530

    Update book.js

--- book.js
+++ book.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const Schema=mongoose.Schema;
 
-const DB = "mongodb+srv://dream:dream@cluster0.uq5mq.mongodb.net/?retryWrites=true&w=majority";
+const DB = "mongodb+srv://dream:dream@cluster0.uq5mq.mongodb.net/Library?retryWrites=true&w=majority";
 mongoose.connect(DB,{ useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
  console.log("Database Connection Successful")
 }).catch((err)=>{
